c787df95ee7e54a724edb2944ad9c3d3ed2e3805
Nathalia Lin <nl1532@nyu.edu>
2019-10-10 00:40:27 +0800

    added navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,43 @@
 import * as React from "react";
-import { Switch, Route, HashRouter } from "react-router-dom";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import 'reset-css';
 
 import HomePage from "./pages/home.page";
 import SignupPage from "./pages/signup.page";
 import LoginPage from "./pages/login.page";
+import NavbarComponent from "./components/navbar.component";
+import { Component } from "react";
 
-const App = () => {
-  return (
+const navigation = {
+  brand: {name: 'WellBee', to: '/'},
+  links: [
+    { name: 'About', to: '/about' },
+    { name: 'Contact', to: '/contact' },
+    { name: 'Login', to: '/login' },
+    { name: 'Sign Up', to: '/signup' }
+  ]
+}
+
+class App extends Component {
+  public render() {
+    const { brand, links } = navigation;
+
+    return (
     <>
+      <div className='App'>
+        <NavbarComponent brand={brand} links={links}/>
+      </div>
 
-      <HashRouter>
+      <BrowserRouter>
         <Switch>
           <Route exact={true} path="/signup" component={SignupPage} />
           <Route exact={true} path="/login" component={LoginPage} />
           <Route path="/" component={HomePage} />
         </Switch>
-      </HashRouter>
+      </BrowserRouter>
     </>
-  );
-};
+    )
+  }
+}
 
 export default App;
