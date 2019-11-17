b0dd76c476a35115b5e40645820740e7becebecc
Jason-ZhouZJ <zz1435@nyu.edu>
2019-11-17 21:22:09 +0800

    Added search

--- server/src/controllers/search.ts
+++ server/src/controllers/search.ts
@@ -3,7 +3,6 @@ import { Request, Response, NextFunction } from "express";
 import { User, UserDocument } from "../models/User";
 import { Doctor, DoctorDocument } from "../models/Doctor";
 import { Admin, AdminDocument } from "../models/Admin";
-import { nextTick } from "async";
 
 
 export const postSearch = (req:Request, res: Response, next: NextFunction) => {
@@ -11,13 +10,13 @@ export const postSearch = (req:Request, res: Response, next: NextFunction) => {
 
     //This key word is email;
 
-    User.findOne({email: req.body.keyword}, (err, existingUser: UserDocument) => {
+    User.findOne({email: req.body.keyword}, (err: Error, existingUser: UserDocument) => {
         if (err){ return next(err); }
         if (existingUser){ 
             res.json({
                 name: existingUser.name,
                 email: existingUser.email,
-                age: existingUser
+                age: existingUser.age,
             })
         }
         res.send("User not found.");
