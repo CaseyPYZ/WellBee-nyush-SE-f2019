7d3e487dc8693289c7ce624ac55b71a5eae6ffea
CaseyPYZ <casey.yzpan@gmail.com>
2019-12-07 17:01:09 +0800

    update add record

--- server/src/app.ts
+++ server/src/app.ts
@@ -100,7 +100,7 @@ app.post("/reset/:token", userController.postReset);
 app.get("/signup", userController.getSignup);
 app.post("/signup", signUp.postSignup);
 //app.get("/search", searchController.getSearch);
-app.post("/search", searchController.postSearch);
+//app.post("/search", searchController.postSearch);
 app.get("/contact", contactController.getContact);
 app.post("/contact", contactController.postContact);
 app.get("/account", passportConfig.isAuthenticated, userController.getAccount);
