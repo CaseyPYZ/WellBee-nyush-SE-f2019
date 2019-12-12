ef0321bdfca497b27b2d8e5458ae31451f21eda4
Nathalia Lin <nl1532@nyu.edu>
2019-12-12 10:51:22 +0800

    frontend

--- frontend/src/components/layout/sidebar.tsx
+++ frontend/src/components/layout/sidebar.tsx
@@ -29,8 +29,8 @@ export default class SidebarComponent extends React.Component<any, any> {
           <div className="w3-sidebar w3-light-grey w3-bar-block">
             <h2 className="w3-bar-item"><img src="bee-128.png" alt="" ></img></h2>
             <div className="w3-bar-item w3-button"><Link to="/profile">Profile</Link></div>
-            <div className="w3-bar-item w3-button"><Link to="/doctor/patient-list">Patient List</Link></div>
-            <div className="w3-bar-item w3-button"><Link to="/doctor/request">Request</Link></div>
+            <div className="w3-bar-item w3-button"><Link to="/doctor/patient-list">Search</Link></div>
+            <div className="w3-bar-item w3-button"><Link to="/doctor/request">Authorized</Link></div>
           </div>
         ) : (this.props.loggedInStatus === "LOGGED_IN" && this.props.usertype === "admin" ? (
           <Sidebar className="w3-sidebar w3-light-grey w3-bar-block">
